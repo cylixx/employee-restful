e1644bb4e4bbc5db8713c343f74a21027bba6d08
cylixx <marko.hinojosa@gmail.com>
2020-07-20 15:57:25 -0400

    Add test

--- src/test/java/com/softland/employeeresful/EmployeeResfulApplicationTests.java
+++ src/test/java/com/softland/employeeresful/EmployeeResfulApplicationTests.java
@@ -1,7 +1,14 @@
 package com.softland.employeeresful;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import com.softland.employeeresful.vo.Employee;
 
 @SpringBootTest
 class EmployeeResfulApplicationTests {
@@ -10,4 +17,13 @@ class EmployeeResfulApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void getEmployee() {
+		RestTemplate rt =  new RestTemplate();
+		String url = "http://localhost:8080/employee/1";
+		ResponseEntity<Employee> response = rt.getForEntity(url, Employee.class);
+		//assertThat(response.getStatusCode(), equal(HttpStatus.OK));
+		System.out.println("============ " + response.getBody());
+		assertEquals(response.getStatusCode(), HttpStatus.OK); 
+	}
 }
